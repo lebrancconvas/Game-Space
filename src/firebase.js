5b740d5080a16f195ae70844db3140f1e9b372ba
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-28 06:42:35 +0700

    :sparkles: Connect Firebase to Project.

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyAKQMjopT_b2EfrI6QbRmv6Voq7TTkEhNs",
+    authDomain: "game-space-1412.firebaseapp.com",
+    projectId: "game-space-1412",
+    storageBucket: "game-space-1412.appspot.com",
+    messagingSenderId: "302923654232",
+    appId: "1:302923654232:web:fe215e26fc4abe2434962d"
+}
+
+firebase.initializeApp(config)
